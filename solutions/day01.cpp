67c88206ff8ae9e725be6ecca4cfd00b3f992087
Zac <zacariatawfick@gmail.com>
2025-12-01 00:22:20 -0500

    Day 1.

--- solutions/day01.cpp
+++ solutions/day01.cpp
@@ -4,6 +4,24 @@ using namespace std;
 
 int main() {
     ifstream file("../input/day01.txt");
-
+    int dial = 50;
+    int passDial = 50;
+    int zeroCount = 0;
+    int passZeroCount = 0;
+    string line;
+    while (file >> line) {
+        char dir = line[0];
+        int mag = stoi(line.substr(1));
+        dial = (dial + (dir == 'R' ? mag : -mag)) % 100;
+        zeroCount += (dial == 0);
+        while (mag > 0) {
+            passDial += dir == 'R' ? 1 : -1;
+            if (passDial == 0 || passDial % 100 == 0)
+                ++passZeroCount;
+            --mag;
+        }
+    }
+    cout << "Phase 1 ans: " << zeroCount << "\n";
+    cout << "Phase 2 ans: " << passZeroCount << "\n";
     return 0;
 }
