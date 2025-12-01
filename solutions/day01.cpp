160cf6f22e10a480bac322fd390b550de479676d
Zac <zacariatawfick@gmail.com>
2025-12-01 01:01:28 -0500

    Clceaned up solution.

--- solutions/day01.cpp
+++ solutions/day01.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+int passes(int start, int end) { return abs((end / 100) - (start / 100)); }
+
 int main() {
     ifstream file("../input/day01.txt");
     int dial = 50;
@@ -11,17 +13,13 @@ int main() {
     string line;
     while (file >> line) {
         char dir = line[0];
-        int mag = stoi(line.substr(1));
-        dial = (dial + (dir == 'R' ? mag : -mag)) % 100;
+        int mag = (dir == 'R' ? 1 : -1) * stoi(line.substr(1));
+        passZeroCount += passes(passDial, passDial + mag);
+        passDial += mag;
+        dial = (mag + dial) % 100;
         zeroCount += (dial == 0);
-        while (mag > 0) {
-            passDial += dir == 'R' ? 1 : -1;
-            if (passDial == 0 || passDial % 100 == 0)
-                ++passZeroCount;
-            --mag;
-        }
     }
     cout << "Phase 1 ans: " << zeroCount << "\n";
-    cout << "Phase 2 ans: " << passZeroCount << "\n";
+    cout << "Phase 2 ans: " << passZeroCount - 1 << "\n";
     return 0;
 }
