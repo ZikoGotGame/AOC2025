6a6f6ab79e1541470ba1cdf6fe5c1a9cf64d34a6
Zac <zacariatawfick@gmail.com>
2025-12-01 22:51:20 -0500

    Clceaned up solution for day 1.

--- solutions/day01.cpp
+++ solutions/day01.cpp
@@ -2,24 +2,34 @@
 
 using namespace std;
 
-int passes(int start, int end) { return abs((end / 100) - (start / 100)); }
+int floor(int a, int b) {
+    if (a >= 0)
+        return a / b;
+    return -((-a + b - 1) / b);
+}
+
+int get_passes(int dial, int mag) {
+    int sum = dial + mag;
+    if (mag > 0)
+        return floor(sum, 100) - floor(dial, 100);
+    else if (mag < 0)
+        return floor(dial - 1, 100) - floor(sum - 1, 100);
+    return 0;
+}
 
 int main() {
     ifstream file("../input/day01.txt");
     int dial = 50;
-    int passDial = 50;
     int zeroCount = 0;
     int passZeroCount = 0;
     string line;
     while (file >> line) {
-        char dir = line[0];
-        int mag = (dir == 'R' ? 1 : -1) * stoi(line.substr(1));
-        passZeroCount += passes(passDial, passDial + mag);
-        passDial += mag;
+        int mag = (line[0] == 'R' ? 1 : -1) * stoi(line.substr(1));
+        passZeroCount += get_passes(dial, mag);
         dial = (mag + dial) % 100;
         zeroCount += (dial == 0);
     }
     cout << "Phase 1 ans: " << zeroCount << "\n";
-    cout << "Phase 2 ans: " << passZeroCount - 1 << "\n";
+    cout << "Phase 2 ans: " << passZeroCount << "\n";
     return 0;
 }
---
+++ solutions/day02.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ifstream file("../input/day02.txt");
+
+    return 0;
+}
