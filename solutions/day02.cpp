f60005c4b81bd8ef75fff0f53831274131cd1493
zac <zacariatawfick@gmail.com>
2025-12-02 06:27:01 -0500

    Day 2 part 2 done.

--- solutions/day02.cpp
+++ solutions/day02.cpp
@@ -2,6 +2,32 @@
 
 using namespace std;
 
+bool is_invalid(string rep) {
+    int n = rep.length();
+    for (int i = 1; i < n; ++i) {
+        if (n % i != 0)
+            continue;
+        string first = rep.substr(0, i);
+        bool match = true;
+        for (int j = i; j + i <= n; j += i)
+            if (first != rep.substr(j, i))
+                match = false;
+        if (match)
+            return true;
+    }
+    return false;
+}
+
+long find_intervals_p2(long start, long end) {
+    long invalid = 0;
+    for (long i = start; i <= end; ++i) {
+        string rep = to_string(i);
+        if (is_invalid(rep))
+            invalid += i;
+    }
+    return invalid;
+}
+
 long find_invalids(long start, long end) {
     long invalid = 0;
     for (long i = start; i <= end; ++i) {
@@ -20,7 +46,7 @@ int main() {
     ifstream file("../input/day02.txt");
     string input;
     file >> input;
-    long invalid = 0;
+    long invalid = 0, all_invalids = 0;
     while (true) {
         int idx = input.find(",");
         string range = idx == string::npos ? input : input.substr(0, idx);
@@ -28,10 +54,11 @@ int main() {
         long start = stol(range.substr(0, split));
         long end = stol(range.substr(split + 1));
         invalid += find_invalids(start, end);
+        all_invalids += find_intervals_p2(start, end);
         if (idx == string::npos)
             break;
         input = input.substr(idx + 1);
     }
-    printf("Phase 1: %ld\n", invalid);
+    printf("Phase 1: %ld\nPhase 2: %ld\n", invalid, all_invalids);
     return EXIT_SUCCESS;
 }
