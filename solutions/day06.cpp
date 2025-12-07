16ba9ad3b4ea84d42f130c6b08fb9614ec240a95
Zac <zacariatawfick@gmail.com>
2025-12-07 01:06:51 -0500

    Solved day 6.

---
+++ solutions/day06.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+#include <cctype>
+
+using namespace std;
+
+long solve_hw(const vector<vector<int>> &values, const vector<char> &operations) {
+    long grand_total = 0;
+    for (int i = 0; i < values[0].size(); ++i) {
+        long column_val = (operations[i] == '*');
+        for (int j = 0; j < values.size(); ++j) {
+            switch (operations[i]) {
+            case '+':
+                column_val += values[j][i];
+                break;
+            default:
+                column_val *= values[j][i];
+            }
+        }
+        grand_total += column_val;
+    }
+    return grand_total;
+}
+
+long solve_hw_p2(const vector<string> &values, const vector<char> &operations) {
+    long grand_total = 0;
+    int idx = operations.size() - 1;
+    long problem = (operations[idx] == '*');
+    for (int i = values[0].length() - 1; i >= 0; --i) {
+        string num = "";
+        for (int j = 0; j < values.size(); ++j)
+            if (!isspace(values[j][i]))
+                num += values[j][i];
+        if (num == "") {
+            grand_total += problem;
+            --idx;
+            problem = (operations[idx] == '*');
+            continue;
+        }
+        long val = stol(num);
+        switch (operations[idx]) {
+        case '+':
+            problem += val;
+            break;
+        default:
+            problem *= val;
+        }
+    }
+    grand_total += problem;
+    return grand_total;
+}
+
+int main() {
+    ifstream file("../input/day06.txt");
+    vector<vector<int>> values;
+    vector<string> values_wspace;
+    for (int i = 0; i < 4; ++i) {
+        string line;
+        vector<int> line_vals;
+        getline(file, line);
+        values_wspace.push_back(line);
+        istringstream stream(line);
+        int num;
+        while (stream >> num)
+            line_vals.push_back(num);
+        values.push_back(line_vals);
+    }
+    vector<char> operations(values[0].size());
+    for (char &c : operations)
+        file >> c;
+    printf("Part 1: %ld\nPart 2: %ld\n", solve_hw(values, operations), solve_hw_p2(values_wspace, operations));
+    return EXIT_SUCCESS;
+}
